5632580f978c55a14af85270bb577dffd6c76d34
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-08-19 19:10:03 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -25,8 +25,10 @@ end
  end 
  
  def best_tipper
-   
-   
+   best_tipped_meal = meals.max dp |meal_a, meal_b|
+   meal_a.tip <=> meal_b.tip 
+  end 
+   best_tipped_meal.customer
  end
    
  end
