94142b8995961f96205147ca5ae3198e14fbe5ac
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-08-19 18:29:11 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,7 +1,11 @@
 class Meal
 
-def initialize()
-  
+def initialize(waiter, customer, total, tip)
+  @waiter = waiter
+  @customer = customer
+  @total = total
+  @tip = tip
+  @@all << self 
 end
 
 end
